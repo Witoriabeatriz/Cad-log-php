34095aea9599b6c4d164e91f2201fd681c0f9e8a
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-06-12 09:03:15 -0300

    Altera titulo das paginas

--- cadastrar.php
+++ cadastrar.php
@@ -49,7 +49,7 @@
   <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Witória Beatriz</h3>
+      <h3 class="float-md-start mb-0">Cadastro</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link" aria-current="page" href="index.php">Início</a>
         <a class="nav-link active" href="cadastrar.php">Cadastrar</a>
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Witória Beatriz</h3>
+      <h3 class="float-md-start mb-0">Início</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link active" aria-current="page" href="index.php">Início</a>
         <a class="nav-link" href="cadastrar.php">Cadastrar</a>
